29fa2ed1631cdc21b8d1f6cc8cea3967c04ccff8
Brady Neal <bradyneal11@gmail.com>
2020-03-30 17:32:02 -0400

    Add separate assignment and outcome linear models

--- main_example.py
+++ main_example.py
@@ -1,12 +1,12 @@
 from pyro.infer.autoguide import AutoNormal
 from DataGenModel import DataGenModel
 from data.synthetic import generate_zty_linear_scalar_data
-from models import linear_gaussian_model
+from models import linear_gaussian_full_model
 
 df = generate_zty_linear_scalar_data(500)
 
-gen_model = DataGenModel(df, linear_gaussian_model, AutoNormal, n_iters=2000)
+gen_model = DataGenModel(df, linear_gaussian_full_model, AutoNormal, n_iters=2000)
 gen_model.plot_ty_dists(n_samples_per_z=10, name='SVI Test')
 
-gen_model2 = DataGenModel(df, linear_gaussian_model, svi=False, mcmc='nuts')
+gen_model2 = DataGenModel(df, linear_gaussian_full_model, svi=False, mcmc='nuts')
 gen_model2.plot_ty_dists(n_samples_per_z=10, name='NUTS Test')
