7cda2cf6fc6f79090eacbe7ad2f1894c1bb35596
Brady Neal <bradyneal11@gmail.com>
2020-04-03 13:52:18 -0400

    Add multivariate z linear data

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import torch
 
-from data.synthetic import generate_zty_linear_scalar_data
+from data.synthetic import generate_zty_linear_scalar_data, generate_zty_linear_multi_z_data
 from data.whynot_simulators import generate_lalonde_random_outcome
 from utils import PANDAS, TORCH
 
@@ -24,3 +24,8 @@ def test_lalonde_random_outcome_data_pandas():
 def test_lalonde_random_outcome_data_torch():
     (z, t, y), causal_effects = generate_lalonde_random_outcome(data_format=TORCH)
     assert isinstance(z, torch.Tensor) and isinstance(t, torch.Tensor) and isinstance(y, torch.Tensor)
+
+
+def test_multivariate_z_data():
+    z, t, y = generate_zty_linear_multi_z_data(10)
+    assert all(isinstance(x, torch.Tensor) for x in (z, t, y))
