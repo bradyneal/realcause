921b2bfe2ec244268ce02dbfcf7b9a308850acd8
Brady Neal <bradyneal11@gmail.com>
2020-04-03 00:31:21 -0400

    Add data format tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,10 +1,26 @@
+import pandas as pd
+import torch
+
 from data.synthetic import generate_zty_linear_scalar_data
 from data.whynot_simulators import generate_lalonde_random_outcome
+from utils import PANDAS, TORCH
 
 
-def test_linear_scalar_data():
-    df = generate_zty_linear_scalar_data(10)
+def test_linear_scalar_data_pandas():
+    df = generate_zty_linear_scalar_data(10, data_format=PANDAS)
+    assert isinstance(df, pd.DataFrame)
 
 
-def test_lalonde_random_outcome_data():
-    df, causal_effects = generate_lalonde_random_outcome()
+def test_linear_scalar_data_torch():
+    z, t, y = generate_zty_linear_scalar_data(10, data_format=TORCH)
+    assert isinstance(z, torch.Tensor) and isinstance(t, torch.Tensor) and isinstance(y, torch.Tensor)
+
+
+def test_lalonde_random_outcome_data_pandas():
+    df, causal_effects = generate_lalonde_random_outcome(data_format=PANDAS)
+    assert isinstance(df, pd.DataFrame)
+
+
+def test_lalonde_random_outcome_data_torch():
+    (z, t, y), causal_effects = generate_lalonde_random_outcome(data_format=TORCH)
+    assert isinstance(z, torch.Tensor) and isinstance(t, torch.Tensor) and isinstance(y, torch.Tensor)
