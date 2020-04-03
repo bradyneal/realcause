dca0a3a6e2cb4a3481ede5b9eef9eccda0dfa012
Brady Neal <bradyneal11@gmail.com>
2020-04-03 18:09:24 -0400

    Add tests for multivariate z model

--- tests/test_DataGenModel.py
+++ tests/test_DataGenModel.py
@@ -3,8 +3,8 @@ from pytest import approx
 
 from pyro.infer.autoguide import AutoNormal
 from DataGenModel import DataGenModel
-from data.synthetic import generate_zty_linear_scalar_data
-from models import linear_gaussian_full_model, linear_gaussian_outcome_model
+from data.synthetic import generate_zty_linear_scalar_data, generate_zty_linear_multi_z_data
+from models import linear_gaussian_full_model, linear_gaussian_outcome_model, linear_multi_z_outcome_model
 from utils import PANDAS, TORCH
 
 
@@ -16,5 +16,14 @@ def linear_scalar_data(request):
 @pytest.mark.parametrize('model', [
     linear_gaussian_full_model, linear_gaussian_outcome_model,
 ])
-def test_fast_model(linear_scalar_data, model):
+def test_fast_scalar_model(linear_scalar_data, model):
     DataGenModel(linear_scalar_data, model, AutoNormal, n_iters=10)
+
+
+@pytest.fixture(scope='module')
+def linear_multi_z_data():
+    return generate_zty_linear_multi_z_data(100, zdim=10)
+
+
+def test_fast_multi_z_model(linear_multi_z_data):
+    DataGenModel(linear_multi_z_data, linear_multi_z_outcome_model, AutoNormal, n_iters=10)
--- tests/test_DataGenModel_training.py
+++ tests/test_DataGenModel_training.py
@@ -6,8 +6,8 @@ from pytest import approx
 
 from pyro.infer.autoguide import AutoNormal
 from DataGenModel import DataGenModel
-from data.synthetic import generate_zty_linear_scalar_data
-from models import linear_gaussian_full_model, linear_gaussian_outcome_model
+from data.synthetic import generate_zty_linear_scalar_data, generate_zty_linear_multi_z_data
+from models import linear_gaussian_full_model, linear_gaussian_outcome_model, linear_multi_z_outcome_model
 
 # Mark all tests in this module as slow
 pytestmark = pytest.mark.slow
@@ -42,3 +42,12 @@ def test_linear_full_model_ate(linear_gen_model_ate):
 def test_linear_outcome_model_ate(linear_gen_model_ate):
     ate_est = linear_gen_model_ate(model=linear_gaussian_outcome_model)
     assert ate_est == approx(ate_est, abs=.1)
+
+
+@pytest.mark.parametrize('ate', [-5, 0, 5])
+def test_linear_multi_z_outcome_model(ate):
+    z, t, y = generate_zty_linear_multi_z_data(500, zdim=10, delta=ate)
+    gen_model = DataGenModel((z, t, y), linear_multi_z_outcome_model, AutoNormal, n_iters=1000)
+    ate_est = gen_model.get_ate()
+    print('expected: {}\t actual: {}'.format(ate, ate_est))
+    assert ate_est == approx(ate, abs=.1)
