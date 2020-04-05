940e094eae711d0cd91156ecf3a65c76654ee1e5
Brady Neal <bradyneal11@gmail.com>
2020-04-05 16:44:36 -0400

    Fix overindent

--- data/synthetic.py
+++ data/synthetic.py
@@ -16,10 +16,10 @@ def generate_zty_linear_multi_z_data(n, zdim=10, data_format=TORCH, seed=0, delt
                                      alpha_mean=0, alpha_std=1, beta_mean=0, beta_std=1,
                                      z_mean=0, z_std=1, t_noise_mean=0, t_noise_std=1,
                                      y_noise_mean=0, y_noise_std=1):
-        np.random.seed(seed)
-        z = np.random.normal(loc=z_mean, scale=z_std, size=(n, zdim))
-        alpha = np.random.normal(loc=alpha_mean, scale=alpha_std, size=(zdim, 1))
-        beta = np.random.normal(loc=beta_mean, scale=beta_std, size=(zdim, 1))
-        t = (z @ alpha).squeeze() + np.random.normal(loc=t_noise_mean, scale=t_noise_std, size=n)
-        y = (delta * t) + (z @ beta).squeeze() + np.random.normal(loc=y_noise_mean, scale=y_noise_std, size=n)
-        return to_data_format(data_format, z, t, y)
+    np.random.seed(seed)
+    z = np.random.normal(loc=z_mean, scale=z_std, size=(n, zdim))
+    alpha = np.random.normal(loc=alpha_mean, scale=alpha_std, size=(zdim, 1))
+    beta = np.random.normal(loc=beta_mean, scale=beta_std, size=(zdim, 1))
+    t = (z @ alpha).squeeze() + np.random.normal(loc=t_noise_mean, scale=t_noise_std, size=n)
+    y = (delta * t) + (z @ beta).squeeze() + np.random.normal(loc=y_noise_mean, scale=y_noise_std, size=n)
+    return to_data_format(data_format, z, t, y)
