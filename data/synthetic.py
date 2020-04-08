77eca5d29725a67143261432bebf89dc38b362fb
Brady Neal <bradyneal11@gmail.com>
2020-04-08 01:17:23 -0400

    Add option for binary treatment data

--- data/synthetic.py
+++ data/synthetic.py
@@ -2,6 +2,12 @@ import numpy as np
 from utils import to_data_format, PANDAS, TORCH
 
 
+# def sigmoid(x):
+#     return np.where(x >= 0,
+#                     1 / (1 + np.exp(-x)),
+#                     np.exp(x) / (1 + np.exp(x)))
+
+
 def generate_zty_linear_scalar_data(n, data_format=PANDAS, seed=0, alpha=-2, beta=2, delta=2,
                                     z_mean=0, z_std=1, t_noise_mean=0, t_noise_std=1,
                                     y_noise_mean=0, y_noise_std=1):
@@ -12,7 +18,8 @@ def generate_zty_linear_scalar_data(n, data_format=PANDAS, seed=0, alpha=-2, bet
     return to_data_format(data_format, z, t, y)
 
 
-def generate_zty_linear_multi_z_data(n, zdim=10, data_format=TORCH, seed=0, delta=2,
+def generate_zty_linear_multi_z_data(n, zdim=10, data_format=TORCH,
+                                     binary_treatment=False, seed=0, delta=2,
                                      alpha_mean=0, alpha_std=1, beta_mean=0, beta_std=1,
                                      z_mean=0, z_std=1, t_noise_mean=0, t_noise_std=1,
                                      y_noise_mean=0, y_noise_std=1):
@@ -21,5 +28,7 @@ def generate_zty_linear_multi_z_data(n, zdim=10, data_format=TORCH, seed=0, delt
     alpha = np.random.normal(loc=alpha_mean, scale=alpha_std, size=(zdim, 1))
     beta = np.random.normal(loc=beta_mean, scale=beta_std, size=(zdim, 1))
     t = (z @ alpha).squeeze() + np.random.normal(loc=t_noise_mean, scale=t_noise_std, size=n)
+    if binary_treatment:
+        t = (t > 0).astype(int)
     y = (delta * t) + (z @ beta).squeeze() + np.random.normal(loc=y_noise_mean, scale=y_noise_std, size=n)
     return to_data_format(data_format, z, t, y)
