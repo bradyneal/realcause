991748b4300286e4341e8051af5e8904031ce9ce
Brady Neal <bradyneal11@gmail.com>
2020-04-09 01:01:49 -0400

    Change benchmark results output format to DataFrame and remove print statements

--- run_benchmark.py
+++ run_benchmark.py
@@ -4,6 +4,7 @@ from data.synthetic import generate_zty_linear_scalar_data, generate_zty_linear_
 from data.whynot_simulators import generate_lalonde_random_outcome
 from models import linear_gaussian_full_model, linear_gaussian_outcome_model, linear_multi_z_outcome_model, VAE
 import numpy as np
+import pandas as pd
 import whynot as wn
 from utils import NUMPY
 
@@ -34,22 +35,17 @@ def get_whynot_estimator_suite_stats(n_seeds, n_samples=100, ate=2):
         assert estimates_data.keys() == estimated_effects.keys()
         for key, estimate in estimated_effects.items():
             estimates_data[key]['ate_ests'][seed] = estimate.ate
-            print("{} < {} < {}".format(estimate.ci[0], ate, estimate.ci[1]))
             if estimate.ci[0] < ate < estimate.ci[1]:
-                print('TRUE')
                 estimates_data[key]['coverate_count'] += 1
 
     assert stats.keys() == estimates_data.keys()
     for key, estimator_data in estimates_data.items():
         avg_ate_est = np.mean(estimator_data['ate_ests'])
-        print('avg_ate:', avg_ate_est)
-        print('ests:', estimator_data['ate_ests'])
-        print('square diffs:', np.square(estimator_data['ate_ests'] - ate))
         stats[key]['mse'] = np.mean(np.square(estimator_data['ate_ests'] - ate))
         stats[key]['bias'] = avg_ate_est - ate
         stats[key]['variance'] = np.mean(np.square(estimator_data['ate_ests'] - avg_ate_est))
         stats[key]['coverage_prob'] = estimator_data['coverate_count'] / n_seeds
-    return stats
+    return pd.DataFrame.from_dict(stats, orient='index')
 
 
 if __name__ == '__main__':
