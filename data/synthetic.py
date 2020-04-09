9e25abd29b62a90bdf30a12e2f2ba33f983ca96c
Brady Neal <bradyneal11@gmail.com>
2020-04-09 00:36:35 -0400

    Add options to make scalar synthetic data binary treatment and allow covariates to be 2D

--- data/synthetic.py
+++ data/synthetic.py
@@ -2,19 +2,20 @@ import numpy as np
 from utils import to_data_format, PANDAS, TORCH
 
 
-# def sigmoid(x):
-#     return np.where(x >= 0,
-#                     1 / (1 + np.exp(-x)),
-#                     np.exp(x) / (1 + np.exp(x)))
-
-
-def generate_zty_linear_scalar_data(n, data_format=PANDAS, seed=0, alpha=-2, beta=2, delta=2,
+def generate_zty_linear_scalar_data(n, data_format=PANDAS, binary_treatment=False,
+                                    z_2d=False, seed=0, alpha=-2, beta=2, delta=2,
                                     z_mean=0, z_std=1, t_noise_mean=0, t_noise_std=1,
                                     y_noise_mean=0, y_noise_std=1):
     np.random.seed(seed)
-    z = np.random.normal(loc=z_mean, scale=z_std, size=n)
-    t = (alpha * z) + np.random.normal(loc=t_noise_mean, scale=t_noise_std, size=n)
-    y = (delta * t) + (beta * z) + np.random.normal(loc=y_noise_mean, scale=y_noise_std, size=n)
+    if z_2d:
+        size = (n, 1)
+    else:
+        size = n
+    z = np.random.normal(loc=z_mean, scale=z_std, size=size)
+    t = (alpha * z) + np.random.normal(loc=t_noise_mean, scale=t_noise_std, size=size)
+    if binary_treatment:
+        t = t > 0
+    y = (delta * t) + (beta * z) + np.random.normal(loc=y_noise_mean, scale=y_noise_std, size=size)
     return to_data_format(data_format, z, t, y)
 
 
@@ -29,6 +30,6 @@ def generate_zty_linear_multi_z_data(n, zdim=10, data_format=TORCH,
     beta = np.random.normal(loc=beta_mean, scale=beta_std, size=(zdim, 1))
     t = (z @ alpha).squeeze() + np.random.normal(loc=t_noise_mean, scale=t_noise_std, size=n)
     if binary_treatment:
-        t = (t > 0).astype(int)
+        t = t > 0
     y = (delta * t) + (z @ beta).squeeze() + np.random.normal(loc=y_noise_mean, scale=y_noise_std, size=n)
     return to_data_format(data_format, z, t, y)
