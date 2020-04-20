eec4b266619bad2bc130886250bc778e36f2647f
Brady Neal <bradyneal11@gmail.com>
2020-04-20 16:08:36 -0400

    Condition VAE encoder, change exp std parameterization to softplus, and plot VAE distribution to aid debugging

--- examples.py
+++ examples.py
@@ -3,6 +3,7 @@ from DataGenModel import DataGenModel
 from data.synthetic import generate_zty_linear_scalar_data, generate_zty_linear_multi_z_data
 from data.whynot_simulators import generate_lalonde_random_outcome
 from models import linear_gaussian_full_model, linear_gaussian_outcome_model, linear_multi_z_outcome_model, VAE
+import numpy as np
 
 df = generate_zty_linear_scalar_data(500)
 
@@ -21,12 +22,14 @@ gen_model4 = DataGenModel(df2, linear_gaussian_outcome_model, AutoNormal, lr=.03
 print('Expected ATE: 5\t Actual: ', gen_model4.get_interventional_mean(1) - gen_model4.get_interventional_mean(0))
 print('Expected ATE: 5\t Actual: ', gen_model4.get_ate())
 
-zdim = 10
+
+zdim = 5
 (z, t, y) = generate_zty_linear_multi_z_data(100, zdim=zdim, delta=10)
 gen_model = DataGenModel((z, t, y), linear_multi_z_outcome_model, AutoNormal, lr=.03, n_iters=2500)
 print('Expected ATE: 10\t Actual: ', gen_model.get_ate())
 
 zdim = 5
 (z, t, y) = generate_zty_linear_multi_z_data(100, zdim=zdim, delta=10)
-vae = VAE(w_dim=zdim)
+vae = VAE(w_dim=zdim, debug=False)
 gen_model = DataGenModel((z, t, y), vae.model, vae.guide, lr=.001, n_iters=20000)
+gen_model.plot_ty_dists(name='VAE', joint=True, marginal_hist=True, marginal_qq=True)
--- models/vae.py
+++ models/vae.py
@@ -3,76 +3,64 @@ import pyro.distributions as dist
 from pyro.nn import PyroModule, PyroSample, PyroParam
 import torch
 from torch import nn
+import torch.nn.functional as F
 
+
+# TODO: maybe try a linear VAE
 # Note: below, w denotes the confounder, t denotes the treatment, and z denotes the latent variable for generation
 class Decoder(nn.Module):
-    def __init__(self, z_dim, hidden_dim, w_dim, t_dim=1, y_dim=1):
+    def __init__(self, z_dim, hidden_dim, y_dim=1, debug=False):
         super().__init__()
-        assert z_dim > 1 and w_dim > 1 and t_dim == 1 and y_dim == 1
-        self.z_to_h = nn.Linear(z_dim + w_dim + t_dim, hidden_dim)
+        self.z_to_h = nn.Linear(z_dim, hidden_dim)
         self.h_to_ymean = nn.Linear(hidden_dim, y_dim)
         self.h_to_ystd = nn.Linear(hidden_dim, y_dim)
-        self.activation = nn.Softplus()
+        self.activation = nn.ReLU()
+        # self.activation = nn.Softplus()
+        self.debug = debug
 
-    def forward(self, z, w, t):
-        assert z.ndim == 2
-        assert w.ndim == 2
-        assert t.ndim == 1
-        zwt = torch.cat((z, w, t.unsqueeze(dim=1)), dim=1)
-        hidden = self.activation(self.z_to_h(zwt))
+    def forward(self, z):
+        hidden = self.activation(self.z_to_h(z))
         y_mean = self.h_to_ymean(hidden)
-        y_std = torch.abs(self.h_to_ystd(hidden))
-        y_std = torch.min(y_std, torch.Tensor([100]))
-        # print('y_std\tmin: {} ... max {}'.format(y_std.min(), y_std.max()))
+        y_std = F.softplus(self.h_to_ystd(hidden))
 
-        # y_std = torch.ones(y_std.shape[0])
+        if self.debug:
+            print('y_mean\tmin: {} ... max {}'.format(y_mean.min(), y_mean.max()))
+            print('y_std\tmin: {} ... max {}'.format(y_std.min(), y_std.max()))
         return y_mean, y_std
 
 
 class Encoder(nn.Module):
-    def __init__(self, z_dim, hidden_dim, w_dim, t_dim):
+    def __init__(self, z_dim, hidden_dim, y_dim, debug=False):
         super().__init__()
-        assert z_dim > 1 and t_dim == 1
-        self.y_to_h = nn.Linear(1, hidden_dim)
+        self.y_to_h = nn.Linear(y_dim, hidden_dim)
         self.h_to_zmean = nn.Linear(hidden_dim, z_dim)
         self.h_to_zstd = nn.Linear(hidden_dim, z_dim)
-        # self.h_to_wmean = nn.Linear(hidden_dim, w_dim)
-        # self.h_to_wstd = nn.Linear(hidden_dim, w_dim)
-        # self.h_to_tmean = nn.Linear(hidden_dim, t_dim)
-        # self.h_to_tstd = nn.Linear(hidden_dim, t_dim)
-        self.activation = nn.Softplus()
+        self.activation = nn.ReLU()
+        # self.activation = nn.Softplus()
+        self.debug = debug
 
     def forward(self, y):
-        assert y.ndim == 1
-        hidden = self.activation(self.y_to_h(y.unsqueeze(dim=1)))
-
+        hidden = self.activation(self.y_to_h(y))
         z_mean = self.h_to_zmean(hidden)
-        z_std = torch.abs(self.h_to_zstd(hidden))
-        z_std = torch.min(z_std, torch.Tensor([100]))
-        # print('z_std\tmin: {} ... max {}'.format(z_std.min(), z_std.max()))
+        z_std = F.softplus(self.h_to_zstd(hidden))
 
-        # w_mean = self.h_to_wmean(hidden)
-        # w_std = torch.exp(self.h_to_wstd(hidden))
-        #
-        # t_mean = self.h_to_tmean(hidden)
-        # t_std = torch.exp(self.h_to_tstd(hidden))
-
-        # return (z_mean, z_std), (w_mean, w_std), (t_mean, t_std)
-
+        if self.debug:
+            print('z_mean\tmin: {} ... max {}'.format(z_mean.min(), z_mean.max()))
+            print('z_std\tmin: {} ... max {}'.format(z_std.min(), z_std.max()))
         return z_mean, z_std
 
 
-# TODO: VAE class is currently copy and pasted
+# NOTE: VAE class is mostly copy and pasted from Pyro VAE tutorial
 class VAE(nn.Module):
-    # by default our latent space is 50-dimensional
-    # and we use 400 hidden units
-    def __init__(self, hidden_dim=400, z_dim=50, w_dim=10, t_dim=1, y_dim=1, seed=0, use_cuda=False):
+    def __init__(self, hidden_dim=400, z_dim=50, w_dim=10, t_dim=1, y_dim=1, seed=0, use_cuda=False, debug=False):
         super().__init__()
         pyro.set_rng_seed(seed)
 
         # create the encoder and decoder networks
-        self.encoder = Encoder(hidden_dim=hidden_dim, z_dim=z_dim, w_dim=w_dim, t_dim=t_dim)
-        self.decoder = Decoder(hidden_dim=hidden_dim, z_dim=z_dim, w_dim=w_dim, t_dim=t_dim, y_dim=y_dim)
+        ywt_dim = y_dim + w_dim + t_dim
+        zwt_dim = z_dim + w_dim + t_dim
+        self.encoder = Encoder(y_dim=ywt_dim, hidden_dim=hidden_dim, z_dim=z_dim, debug=debug)
+        self.decoder = Decoder(z_dim=zwt_dim, hidden_dim=hidden_dim, y_dim=y_dim, debug=debug)
 
         if use_cuda:
             # calling cuda() here will put all the parameters of
@@ -81,7 +69,7 @@ class VAE(nn.Module):
         self.use_cuda = use_cuda
         self.z_dim = z_dim
 
-    # define the model p(x|z)p(z)
+    # define the model p(y|z,w,t)p(z,w,t) = p(y|z,w,t)p(z)
     def model(self, w, t, y=None):
         # register PyTorch module `decoder` with Pyro
         pyro.module("decoder", self.decoder)
@@ -92,26 +80,25 @@ class VAE(nn.Module):
             # sample from prior (value will be sampled by guide when computing the ELBO)
             z = pyro.sample("latent", dist.Normal(z_loc, z_scale).to_event(1))
             # decode the latent code z
-            y_mean, y_std = self.decoder.forward(z, w, t)
-            # score against actual images
+            assert z.ndim == 2 and w.ndim == 2 and t.ndim == 1
+            zwt = torch.cat((z, w, t.unsqueeze(dim=1)), dim=1)
+            y_mean, y_std = self.decoder.forward(zwt)
+            # score against actual y
             pyro.sample("y_obs", dist.Normal(y_mean, y_std).to_event(1), obs=y)
 
-    # define the guide (i.e. variational distribution) q(z|x)
-    def guide(self, w, t, y):
+    # define the guide (i.e. variational distribution) q(z|y,w,t)
+    def guide(self, w, t, y=None):
         # register PyTorch module `encoder` with Pyro
         pyro.module("encoder", self.encoder)
         with pyro.plate("data", w.shape[0]):
-            # use the encoder to get the parameters used to define q(z|x)
-            (z_mean, z_std) = self.encoder.forward(y)
-            # sample the latent code z
-            pyro.sample("latent", dist.Normal(z_mean, z_std).to_event(1))
-
-    # # define a helper function for reconstructing images
-    # def reconstruct_img(self, x):
-    #     # encode image x
-    #     z_loc, z_scale = self.encoder(x)
-    #     # sample in latent space
-    #     z = dist.Normal(z_loc, z_scale).sample()
-    #     # decode the image (note we don't sample in image space)
-    #     loc_img = self.decoder(z)
-    #     return loc_img
+            if y is None:   # Evaluation mode for using Pyro Predictive class
+                z_loc = w.new_zeros(torch.Size((w.shape[0], self.z_dim)))
+                z_scale = w.new_ones(torch.Size((w.shape[0], self.z_dim)))
+                pyro.sample("latent", dist.Normal(z_loc, z_scale).to_event(1))
+            else:           # Regular training model
+                # use the encoder to get the parameters used to define q(z|y,w,t)
+                assert y.ndim == 1 and w.ndim == 2 and t.ndim == 1
+                ywt = torch.cat((y.unsqueeze(dim=1), w, t.unsqueeze(dim=1)), dim=1)
+                (z_mean, z_std) = self.encoder.forward(ywt)
+                # sample the latent code z
+                pyro.sample("latent", dist.Normal(z_mean, z_std).to_event(1))
