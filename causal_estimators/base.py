6dbed9483c3f870729d65c64593de7dca338ae1f
Brady Neal <bradyneal11@gmail.com>
2020-05-12 13:26:48 -0400

    Add causal estimators interface

---
+++ causal_estimators/base.py
@@ -0,0 +1,39 @@
+from abc import ABC, abstractmethod
+
+
+class BaseEstimator(ABC):
+
+    @abstractmethod
+    def fit(self, w, t, y):
+        pass
+
+    @abstractmethod
+    def estimate_ate(self, t1=1, t0=0, w=None):
+        pass
+
+    @abstractmethod
+    def ate_conf_int(self, percentile=.95) -> tuple:
+        pass
+
+
+class BaseHeterogeneousEstimator(BaseEstimator):
+
+    @abstractmethod
+    def fit(self, w, t, y):
+        pass
+
+    @abstractmethod
+    def predict_outcome(self, t, w):
+        pass
+
+    def estimate_ate(self, t1=1, t0=0, w=None):
+        return self.estimate_ite(t1=t1, t0=t0, w=w).mean()
+
+    @abstractmethod
+    def ate_conf_int(self, percentile=.95):
+        pass
+
+    def estimate_ite(self, t1=1, t0=0, w=None):
+        return self.predict_outcome(t=t1, w=w) - self.predict_outcome(t=t0, w=w)
+
+    # TODO: ITE confidence interval
