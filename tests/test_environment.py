dd26b70cf9c54a906cfd99fca464ee2ce6d5d604
Brady Neal <bradyneal11@gmail.com>
2020-05-12 18:10:58 -0400

    Add tests for environment

---
+++ tests/test_environment.py
@@ -0,0 +1,35 @@
+def test_matplotlib():
+    import matplotlib
+
+
+def test_numpy():
+    import numpy
+
+
+def test_pandas():
+    import pandas
+
+
+def test_python():
+    import sys
+    assert sys.version_info[0] >= 3
+
+
+def test_pytorch():
+    import torch
+
+
+def test_seaborn():
+    import seaborn
+
+
+def test_statsmodels():
+    import statsmodels
+
+
+def test_pot():
+    import ot
+
+
+def test_torch_two_sample():
+    import torch_two_sample
