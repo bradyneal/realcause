f9288a7f4504706a2692aedacc19fd103477281c
Brady Neal <bradyneal11@gmail.com>
2020-06-04 22:55:03 -0400

    Refactor causallib ITE estimator classes into single BaseCausallibIteEstimator class

--- causal_estimators/base.py
+++ causal_estimators/base.py
@@ -1,6 +1,9 @@
 from abc import ABC, abstractmethod
 from copy import deepcopy
 
+from utils import to_pandas
+from exceptions import NotFittedError
+
 
 class BaseEstimator(ABC):
 
@@ -42,3 +45,48 @@ class BaseIteEstimator(BaseEstimator):
         pass
 
     # TODO: ITE confidence interval
+
+
+class BaseCausallibIteEstimator(BaseIteEstimator):
+
+    def __init__(self, causallib_estimator):
+        self.causallib_estimator = causallib_estimator
+        self.w = None
+        self.t = None
+        self.y = None
+
+    def fit(self, w, t, y):
+        w, t, y = to_pandas(w, t, y)
+        self.causallib_estimator.fit(w, t, y)
+        self.w = w
+        self.t = t
+        self.y = y
+
+    def predict_outcome(self, t, w):
+        return self.causallib_estimator.estimate_individual_outcome(w, t)
+
+    # def estimate_ate(self, t1=1, t0=0, w=None, t=None, y=None):
+    #     w = self.w if w is None else w
+    #     t = self.t if t is None else t
+    #     y = self.y if y is None else y
+    #     if w is None or t is None:
+    #         raise RuntimeError('Must run .fit(w, t, y) before running .estimate_ate()')
+    #     w, t, y = to_pandas(w, t, y)
+    #     mean_potential_outcomes = self.causallib_estimator.estimate_population_outcome(w, t, agg_func="mean")
+    #     ate_estimate = mean_potential_outcomes[1] - mean_potential_outcomes[0]
+    #     return ate_estimate
+
+    def ate_conf_int(self, percentile=.95):
+        # TODO
+        raise NotImplementedError
+
+    def estimate_ite(self, t1=1, t0=0, w=None, t=None, y=None):
+        w = self.w if w is None else w
+        t = self.t if t is None else t
+        y = self.y if y is None else y
+        if w is None or t is None:
+            raise NotFittedError('Must run .fit(w, t, y) before running .estimate_ate()')
+        w, t, y = to_pandas(w, t, y)
+        individual_potential_outcomes = self.causallib_estimator.estimate_individual_outcome(w, t)
+        ite_estimates = individual_potential_outcomes[1] - individual_potential_outcomes[0]
+        return ite_estimates
--- causal_estimators/doubly_robust_estimator.py
+++ causal_estimators/doubly_robust_estimator.py
@@ -2,7 +2,7 @@ from sklearn.linear_model import LinearRegression, LogisticRegression
 
 from causallib.estimation import Standardization, StratifiedStandardization, IPW,\
     DoublyRobustVanilla, DoublyRobustIpFeature, DoublyRobustJoffe
-from causal_estimators.base import BaseIteEstimator
+from causal_estimators.base import BaseCausallibIteEstimator
 from causal_estimators.standardization_estimator import \
     StandardizationEstimator, StratifiedStandardizationEstimator
 from causal_estimators.ipw_estimator import IPWEstimator
@@ -25,7 +25,7 @@ STR_TO_STANDARDIZATION = {
 TRIM_EPS = 0.01
 
 
-class DoublyRobustEstimator(BaseIteEstimator):
+class DoublyRobustEstimator(BaseCausallibIteEstimator):
 
     def __init__(self, outcome_model=LinearRegression(),
                  prop_score_model=LogisticRegression(),
@@ -45,43 +45,10 @@ class DoublyRobustEstimator(BaseIteEstimator):
         ipw = IPW(learner=prop_score_model, truncate_eps=trim_eps, use_stabilized=stabilized)
 
         standardization = STR_TO_STANDARDIZATION[standardization_type](outcome_model)
-        self.doubly_robust = STR_TO_DOUBLY_ROBUST[doubly_robust_type](
+        doubly_robust = STR_TO_DOUBLY_ROBUST[doubly_robust_type](
             outcome_model=standardization, weight_model=ipw)
-        self.w = None
-        self.t = None
-        self.y = None
 
-    def fit(self, w, t, y):
-        w, t, y = to_pandas(w, t, y)
-        self.doubly_robust.fit(w, t, y)
-        self.w = w
-        self.t = t
-        self.y = y
+        super().__init__(causallib_estimator=doubly_robust)
 
-    def predict_outcome(self, w, t):
-        return self.doubly_robust.estimate_individual_outcome(w, t)
 
-    # def estimate_ate(self, t1=1, t0=0, w=None, t=None, y=None):
-    #     w = self.w if w is None else w
-    #     t = self.t if t is None else t
-    #     y = self.y if y is None else y
-    #     if w is None or t is None:
-    #         raise RuntimeError('Must run .fit(w, t, y) before running .estimate_ate()')
-    #     w, t, y = to_pandas(w, t, y)
-    #     mean_potential_outcomes = self.doubly_robust.estimate_population_outcome(w, t, agg_func="mean")
-    #     ate_estimate = mean_potential_outcomes[1] - mean_potential_outcomes[0]
-    #     return ate_estimate
-
-    def ate_conf_int(self, percentile=.95) -> tuple:
-        raise NotImplementedError
-
-    def estimate_ite(self, t1=1, t0=0, w=None, t=None, y=None):
-        w = self.w if w is None else w
-        t = self.t if t is None else t
-        y = self.y if y is None else y
-        if w is None or t is None:
-            raise RuntimeError('Must run .fit(w, t, y) before running .estimate_ate()')
-        w, t, y = to_pandas(w, t, y)
-        individual_potential_outcomes = self.doubly_robust.estimate_individual_outcome(w, t)
-        ite_estimates = individual_potential_outcomes[1] - individual_potential_outcomes[0]
-        return ite_estimates
+DoublyRobustEstimator()
