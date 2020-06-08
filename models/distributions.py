eea1e5837c6197c1b984f11f53a2f83e74eedb75
CW-Huang <cw.huang427@gmail.com>
2020-06-08 19:15:46 -0400

    modulate distributions

---
+++ models/distributions.py
@@ -0,0 +1,197 @@
+import numpy as np
+import torch
+from torch.nn import functional as F
+
+
+Log2PI = float(np.log(2 * np.pi))
+
+
+def log_normal(x, mean, log_var, eps=0.00001):
+    z = - 0.5 * Log2PI
+    return - (x - mean) ** 2 / (2. * torch.exp(log_var) + eps) - log_var / 2. + z
+
+
+def log_log_logistic(x, log_alpha, log_beta_):
+    """log density of log-logistic with alpha > 0 and beta > 1 (log beta > 0)
+    log_beta_ is reparameterization of the shape parameter, beta = exp(softplus(log_beta_)) > 1
+              which ensures the distribution has a well-defined expected value.
+    see https://en.wikipedia.org/wiki/Log-logistic_distribution for more information
+    support: x > 0
+    """
+    log_beta = F.softplus(log_beta_)
+    beta = torch.exp(log_beta)
+    log_x = torch.log(x)
+    return log_beta + (beta - 1) * log_x - beta * log_alpha - 2 * F.softplus(beta * (log_x - log_alpha))
+
+
+def log_mixed_log_logistic(x, logit_pi, log_alpha, log_beta_):
+    """log likelihood of the following mixed distribution:
+    with probability pi = sigmoid(logit_pi), x follows log-logistic(alpha, exp(softplut(log_beta_)))
+    with probability 1-pi, x = 0
+    """
+    xp = x[x > 0]
+    ll = torch.zeros_like(x)
+    ll[x == 0] = F.logsigmoid(-logit_pi)[x == 0]
+    ll[x > 0] = F.logsigmoid(logit_pi)[x > 0] + log_log_logistic(xp, log_alpha[x > 0], log_beta_[x > 0])
+    return ll
+
+
+def binary_cross_entropy(p, t):
+    """p: prediction, t: target"""
+    return F.binary_cross_entropy_with_logits(p, t)
+
+
+def logistic_sampler(mu, s):
+    """https://en.wikipedia.org/wiki/Logistic_distribution"""
+    u = torch.rand_like(mu)
+    x = torch.log(u) - torch.log(1-u)
+    return mu + s * x
+
+
+def log_logistic_sampler(log_alpha, log_beta_):
+    """log-logistic with alpha > 0 and beta > 1 (log beta > 0)
+    log_beta_ is reparameterization of the shape parameter, beta = exp(softplus(log_beta_)) > 1
+    https://en.wikipedia.org/wiki/Log-logistic_distribution
+    """
+    return torch.exp(logistic_sampler(log_alpha, torch.exp(-F.softplus(log_beta_))))
+
+
+def mixed_log_logistic_sampler(logit_pi, log_alpha, log_beta_):
+    """
+    with probability pi = sigmoid(logit_pi), x follows log-logistic(alpha, exp(softplut(log_beta_)))
+    with probability 1-pi, x = 0
+    """
+    pi = torch.sigmoid(logit_pi)
+    y = torch.zeros_like(pi)
+    yp_ind = pi > torch.rand_like(pi)
+    y[yp_ind] = log_logistic_sampler(log_alpha[yp_ind], log_beta_[yp_ind])
+    return y
+
+
+def gaussian_sampler(mean, log_var):
+    sigma = torch.exp(0.5*log_var)
+    return torch.randn(*mean.shape) * sigma + mean
+
+
+class BaseDistribution(object):
+    """Distribution with batchified likelihood function and sampling function"""
+    def likelihood(self, x, params):
+        raise NotImplementedError
+
+    def sample(self, params):
+        raise NotImplementedError
+
+    def loss(self, x, params):
+        return - self.likelihood(x, params).mean()
+
+    @property
+    def num_params(self):
+        raise NotImplementedError
+
+
+class Bernoulli(BaseDistribution):
+    def likelihood(self, x, params):
+        return - F.binary_cross_entropy_with_logits(params, x, reduction='none').sum(-1)
+
+    def sample(self, params):
+        return (torch.sigmoid(params) > torch.rand_like(params)).float().data.cpu().numpy()
+
+    @property
+    def num_params(self):
+        return 1
+
+
+class FactorialGaussian(BaseDistribution):
+    def likelihood(self, x, params):
+        return log_normal(x, *torch.chunk(params, chunks=2, dim=1)).sum(-1)
+
+    def sample(self, params):
+        return gaussian_sampler(*torch.chunk(params, chunks=2, dim=1)).data.cpu().numpy()
+
+    @property
+    def num_params(self):
+        return 2
+
+
+class LogLogistic(BaseDistribution):
+    def likelihood(self, x, params):
+        return log_log_logistic(x, *torch.chunk(params, chunks=2, dim=1)).sum(-1)
+
+    def sample(self, params):
+        return log_logistic_sampler(*torch.chunk(params, chunks=2, dim=1)).data.cpu().numpy()
+
+    @property
+    def num_params(self):
+        return 2
+
+
+class MixedDistribution(BaseDistribution):
+    def __init__(self, atoms: list, dist: BaseDistribution):
+        """
+        :param atoms: position of the point mass
+        :param dist: continuous distribution
+        """
+        self.num_atoms = len(atoms)
+        self.atoms = atoms
+        self.dist = dist
+
+    def likelihood(self, x, params):
+        slc = [slice(None)] * len(params.shape)
+        slc[-1] = slice(0, self.num_atoms + 1)
+        logit_pi = params[slc]
+        log_pi = F.log_softmax(logit_pi, dim=-1)
+
+        log_pi = log_pi.view(-1, self.num_atoms + 1)
+        x_shape = x.shape
+        x = x.view(-1)
+
+        ll = torch.zeros_like(x)
+        ind_atoms = torch.zeros_like(x).bool()
+        for j, atom in enumerate(self.atoms):
+            ind_ = x == atom
+            ll[ind_] = log_pi[ind_][:, j]
+            ind_atoms += ind_
+
+        ind_non_atoms = ~ind_atoms
+        x_continuous = x[ind_non_atoms]
+        ll[ind_non_atoms] = log_pi[ind_non_atoms][:, -1] + \
+            self.dist.likelihood(x_continuous[:, None],
+                                 params[:, self.num_atoms + 1:][ind_non_atoms].view(-1, self.dist.num_params))
+        return ll.view(*x_shape).sum(-1)
+
+    def sample(self, params):
+        p_ = (params.size(1) - (self.num_atoms + 1)) / self.dist.num_params
+        p = int(p_)
+        assert p == p_, 'dimensionality of params is wrong'
+
+        logit_pi = params[:, :self.num_atoms + 1]
+        pi = F.softmax(logit_pi, dim=-1)
+        x = torch.zeros(params.size(0), p).float().to(params.device)
+        u = torch.rand(params.size(0), p).to(params.device)
+
+        cum_pi = pi.cumsum(-1)
+        ind_ = u < cum_pi[:, 0:1]
+        x[ind_] = self.atoms[0]
+
+        for j in range(self.num_atoms-1):
+            atom = self.atoms[j+1]
+            ind_ = u >= cum_pi[:, j:j+1]
+            x[ind_] = atom
+
+        ind_non_atoms = (u >= cum_pi[:, -2:-1]).squeeze()
+        x[ind_non_atoms] = torch.from_numpy(
+            self.dist.sample(params[:, self.num_atoms + 1:][ind_non_atoms])).to(x.device)
+        return x.to(x.device).data.cpu().numpy()
+
+    @property
+    def num_params(self):
+        # num_atoms + 1 classes (last one being continuous)
+        return self.num_atoms + 1 + self.dist.num_params
+
+
+def quick_test():
+    import matplotlib.pyplot as plt
+    dist = MixedDistribution([0, 1.5], FactorialGaussian())
+    dist.likelihood(torch.randn(1000, 1), torch.randn(1000, 5))
+    dist.sample(torch.randn(100, 5))
+    plt.hist(dist.sample(torch.randn(1000, 5)), 20)
--- models/nonlinear.py
+++ models/nonlinear.py
@@ -1,77 +1,10 @@
 import numpy as np
+from models import distributions
 from models.base import BaseGenModel, Preprocess, PlaceHolderTransform
 import torch
 from torch import nn
-from torch.nn import functional as F
 from torch.utils import data
 from itertools import chain
-
-
-Log2PI = float(np.log(2 * np.pi))
-def log_normal(x, mean, log_var, eps=0.00001):
-    z = - 0.5 * Log2PI
-    return - (x - mean) ** 2 / (2. * torch.exp(log_var) + eps) - log_var / 2. + z
-
-
-def log_log_logistic(x, log_alpha, log_beta_):
-    """log density of log-logistic with alpha > 0 and beta > 1 (log beta > 0)
-    log_beta_ is reparameterization of the shape parameter, beta = exp(softplus(log_beta_)) > 1
-              which ensures the distribution has a well-defined expected value.
-    see https://en.wikipedia.org/wiki/Log-logistic_distribution for more information
-    support: x > 0
-    """
-    log_beta = F.softplus(log_beta_)
-    beta = torch.exp(log_beta)
-    log_x = torch.log(x)
-    return log_beta + (beta - 1) * log_x - beta * log_alpha - 2 * F.softplus(beta * (log_x - log_alpha))
-
-
-def log_mixed_log_logistic(x, logit_pi, log_alpha, log_beta_):
-    """log likelihood of the following mixed distribution:
-    with probability pi = sigmoid(logit_pi), x follows log-logistic(alpha, exp(softplut(log_beta_)))
-    with probability 1-pi, x = 0
-    """
-    xp = x[x > 0]
-    ll = torch.zeros_like(x)
-    ll[x == 0] = F.logsigmoid(-logit_pi)[x == 0]
-    ll[x > 0] = F.logsigmoid(logit_pi)[x > 0] + log_log_logistic(xp, log_alpha[x > 0], log_beta_[x > 0])
-    return ll
-
-
-def binary_cross_entropy(p, t):
-    """p: prediction, t: target"""
-    return F.binary_cross_entropy_with_logits(p, t)
-
-
-def logistic_sampler(mu, s):
-    """https://en.wikipedia.org/wiki/Logistic_distribution"""
-    u = torch.rand_like(mu)
-    x = torch.log(u) - torch.log(1-u)
-    return mu + s * x
-
-
-def log_logistic_sampler(log_alpha, log_beta_):
-    """log-logistic with alpha > 0 and beta > 1 (log beta > 0)
-    log_beta_ is reparameterization of the shape parameter, beta = exp(softplus(log_beta_)) > 1
-    https://en.wikipedia.org/wiki/Log-logistic_distribution
-    """
-    return torch.exp(logistic_sampler(log_alpha, torch.exp(-F.softplus(log_beta_))))
-
-
-def mixed_log_logistic_sampler(logit_pi, log_alpha, log_beta_):
-    """
-    with probability pi = sigmoid(logit_pi), x follows log-logistic(alpha, exp(softplut(log_beta_)))
-    with probability 1-pi, x = 0
-    """
-    pi = torch.sigmoid(logit_pi)
-    y = torch.zeros_like(pi)
-    yp_ind = pi > torch.rand_like(pi)
-    y[yp_ind] = log_logistic_sampler(log_alpha[yp_ind], log_beta_[yp_ind])
-    return y
-
-def gaussian_sampler(mean, log_var):
-    sigma = torch.exp(0.5*log_var)
-    return torch.randn(*mean.shape) * sigma + mean
 
 
 class MLPParams:
@@ -193,7 +126,7 @@ class MLP(BaseGenModel):
                  mlp_params_y_tw=MLPParams(),
                  training_params = TrainingParams(),
                  binary_treatment=False,
-                 outcome_distribution='gaussian',
+                 outcome_distribution:distributions.BaseDistribution=distributions.FactorialGaussian(),
                  outcome_min=None,
                  outcome_max=None,
                  w_transform:Preprocess=PlaceHolderTransform(),
@@ -206,6 +139,10 @@ class MLP(BaseGenModel):
                                   y_transform=y_transform)
 
         self.binary_treatment = binary_treatment
+        if binary_treatment: # todo: input?
+            self.treatment_distribution = distributions.Bernoulli()
+        else:
+            self.treatment_distribution = distributions.FactorialGaussian()
         self.outcome_distribution = outcome_distribution
         self.outcome_min = outcome_min
         self.outcome_max = outcome_max
@@ -217,16 +154,9 @@ class MLP(BaseGenModel):
         self.MLP_params_t_w = mlp_params_t_w
         self.MLP_params_y_tw = mlp_params_y_tw
         output_multiplier_t = 1 if binary_treatment else 2
-        if outcome_distribution == 'gaussian':
-            output_multiplier_y = 2
-        elif outcome_distribution == 'log_logistic':
-            output_multiplier_y = 2
-        elif outcome_distribution == 'mixed_log_logistic':
-            output_multiplier_y = 3
-        else:
-            raise('outcome distribution {} not implemented'.format(outcome_distribution))
         self.mlp_t_w = self._build_mlp(self.dim_w, self.dim_t, mlp_params_t_w, output_multiplier_t)
-        self.mlp_y_tw = self._build_mlp(self.dim_w+self.dim_t, self.dim_y, mlp_params_y_tw, output_multiplier_y)
+        self.mlp_y_tw = self._build_mlp(self.dim_w+self.dim_t, self.dim_y, mlp_params_y_tw,
+                                        outcome_distribution.num_params)
 
         # TODO: multiple optimizers ?
         self.training_params = training_params
@@ -236,15 +166,11 @@ class MLP(BaseGenModel):
         )
 
         # TODO: binary treatment -> long data type
-        self.data_loader = data.DataLoader(CausalDataset(self.w_transformed, self.t_transformed, self.y_transformed,
-                                                         # w_transform=w_transform,
-                                                         # t_transform=t_transform,
-                                                         # y_transform=y_transform
-                                                         ),
+        self.data_loader = data.DataLoader(CausalDataset(self.w_transformed, self.t_transformed, self.y_transformed),
                                            batch_size=training_params.batch_size,
                                            shuffle=True)
 
-        self._train()
+        # self._train()
 
     def _matricize(self, data):
         return [np.reshape(d, [d.shape[0], -1]) for d in data]
@@ -257,26 +183,6 @@ class MLP(BaseGenModel):
         hidden_layers += [nn.Linear(dim_h, dim_y * output_multiplier)]
         return nn.Sequential(*hidden_layers)
 
-    def _treatment_loss(self, t_, t):
-        if self.binary_treatment:
-            loss_t = binary_cross_entropy(t_, t)
-        else:
-            loss_t = - log_normal(t, *torch.chunk(t_, chunks=2, dim=1)).sum(1).mean()
-        return loss_t
-
-    def _outcome_loss(self, y_, y):
-        if self.outcome_distribution == 'log_logistic':
-            log_alpha, log_beta_ = torch.chunk(y_, chunks=2, dim=1)
-            loss_y = - log_log_logistic(y, log_alpha, log_beta_).mean()
-        elif self.outcome_distribution == 'mixed_log_logistic':
-            logit_pi, log_alpha, log_beta_ = torch.chunk(y_, chunks=3, dim=1)
-            loss_y = - log_mixed_log_logistic(y, logit_pi, log_alpha, log_beta_).mean()
-        elif self.outcome_distribution in ['gaussian', 'normal']:
-            loss_y = - log_normal(y, *torch.chunk(y_, chunks=2, dim=1)).sum(1).mean()
-        else:
-            raise('outcome distribution {} not implemented'.format(self.outcome_distribution))
-        return loss_y
-
     def _train(self):
         c = 0
         for _ in range(self.training_params.num_epochs):
@@ -284,36 +190,25 @@ class MLP(BaseGenModel):
                 self.optim.zero_grad()
                 t_ = self.mlp_t_w(w)
                 y_ = self.mlp_y_tw(torch.cat([w,t], dim=1))
-                loss_t = self._treatment_loss(t_, t)
-                loss_y = self._outcome_loss(y_, y)
+                loss_t = self.treatment_distribution.loss(t, t_)
+                loss_y = self.outcome_distribution.loss(y, y_)
                 loss = loss_t + loss_y
                 # TODO: learning rate can be separately adjusted by weighting the losses here
                 loss.backward()
                 self.optim.step()
 
                 if self.training_params.verbose and c % self.training_params.print_every_iters == 0:
-                    print("Iteration {}: {}".format(c, loss))
+                    print("Iteration {}: {} {}".format(c, loss_t, loss_y))
                 c += 1
 
     def _sample_t(self, w=None):
         t_ = self.mlp_t_w(torch.from_numpy(w).float())
-        if self.binary_treatment:
-            t_samples = (torch.sigmoid(t_) > torch.rand_like(t_)).float().data.cpu().numpy()
-        else:
-            t_samples = gaussian_sampler(*torch.chunk(t_, chunks=2, dim=1)).data.cpu().numpy()
-        return t_samples
+        return self.treatment_distribution.sample(t_)
 
     def _sample_y(self, t, w=None):
         wt = np.concatenate([w, t], 1)
         y_ = self.mlp_y_tw(torch.from_numpy(wt).float())
-        if self.outcome_distribution in ['normal', 'gaussian']:
-            y_samples = gaussian_sampler(*torch.chunk(y_, chunks=2, dim=1)).data.cpu().numpy()
-        elif self.outcome_distribution == 'mixed_log_logistic':
-            y_samples = mixed_log_logistic_sampler(*torch.chunk(y_, chunks=3, dim=1)).data.cpu().numpy()
-        elif self.outcome_distribution == 'log_logistic':
-            y_samples = log_logistic_sampler(*torch.chunk(y_, chunks=2, dim=1)).data.cpu().numpy()
-        else:
-            raise('outcome distribution {} not implemented'.format(self.outcome_distribution))
+        y_samples = self.outcome_distribution.sample(y_)
 
         if self.outcome_min is not None or self.outcome_max is not None:
             return np.clip(y_samples, self.outcome_min, self.outcome_max)
@@ -346,46 +241,39 @@ if __name__ == '__main__':
     # w, t, y = load_lalonde(rct=True)
     # w, t, y = load_lalonde(obs_version='cps1')
 
-    logit_pi = torch.zeros(1, requires_grad=True)
-    log_alpha = torch.zeros(1, requires_grad=True)
-    log_beta_ = torch.zeros(1, requires_grad=True)
-    y_torch = torch.from_numpy(y/y.max()).float()
+    # logit_pi = torch.zeros(1, requires_grad=True)
+    # log_alpha = torch.zeros(1, requires_grad=True)
+    # log_beta_ = torch.zeros(1, requires_grad=True)
+    param = torch.zeros(1, 4, requires_grad=True)
+    y_torch = torch.from_numpy(y/y.max()).float()[:,None]
+    dist = distributions.MixedDistribution([0.0], distributions.LogLogistic())
     for i in range(500):
-        logit_pi.grad = None
-        log_alpha.grad = None
-        log_beta_.grad = None
-        nll = - log_mixed_log_logistic(y_torch, logit_pi.expand(len(y)),
-                                       log_alpha.expand(len(y)),
-                                       log_beta_.expand(len(y))).mean()
+        param.grad = None
+        nll = - dist.likelihood(y_torch, param.expand(len(y), -1)).mean()
         nll.backward()
-        logit_pi.data.sub_(0.1 * logit_pi.grad.data)
-        log_alpha.data.sub_(0.1 * log_alpha.grad.data)
-        log_beta_.data.sub_(0.1 * log_beta_.grad.data)
+        param.data.sub_(0.01 * param.grad.data)
+        print(i)
 
 
     plt.hist(y / y.max(), 50, density=True, alpha=0.5, range=(0,1))
     n_ = 1000
-    ll = log_mixed_log_logistic(torch.linspace(0, 1, n_),
-                                logit_pi.expand(n_),
-                                log_alpha.expand(n_),
-                                log_beta_.expand(n_))
+    ll = dist.likelihood(torch.linspace(0, 1, n_)[:, None], param.expand(n_, -1))
     plt.plot(np.linspace(0,1,n_), np.exp(ll.data.numpy()), 'x', ms=2)
 
-    y_samples = mixed_log_logistic_sampler(logit_pi.expand(n_),
-                                           log_alpha.expand(n_),
-                                           log_beta_.expand(n_)).data.numpy()
+    y_samples = dist.sample(param.expand(n_, -1))
     plt.hist(y_samples, 50, density=True, alpha=0.5, range=(0,1))
     plt.legend(['data', 'density', 'samples'], loc=1)
 
     mlp = MLP(w, t, y,
               training_params=TrainingParams(lr=0.0005, batch_size=128, num_epochs=500),
               mlp_params_y_tw=MLPParams(n_hidden_layers=2, dim_h=256),
-              binary_treatment=True, outcome_distribution='mixed_log_logistic',
+              binary_treatment=True, outcome_distribution=dist,
               outcome_min=0.0, outcome_max=1.0, seed=1,
               w_transform=Standardize(w), y_transform=Scaling(1/y.max()))
+    mlp._train()
     data_samples = mlp.sample()
     # mlp.plot_ty_dists()
     uni_metrics = mlp.get_univariate_quant_metrics()
     pp.pprint(uni_metrics)
     # multi_ty_metrics = mlp.get_multivariate_quant_metrics(include_w=False, n_permutations=10)
-    # multi_wty_metrics = mlp.get_multivariate_quant_metrics(include_w=True, n_permutations=10)
+    multi_wty_metrics = mlp.get_multivariate_quant_metrics(include_w=True, n_permutations=10)
