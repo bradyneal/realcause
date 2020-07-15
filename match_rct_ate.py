d543e523449cea815d9d5940f59f2f29cd5df7bf
Brady Neal <bradyneal11@gmail.com>
2020-07-15 09:27:29 -0400

    Add file for check if the generative model's ATE matches the RCT ATE

---
+++ match_rct_ate.py
@@ -0,0 +1,40 @@
+import torch
+
+from models import distributions
+from models import preprocess
+from models.nonlinear import MLP, TrainingParams, MLPParams
+from data.lalonde import load_lalonde
+
+dataset = 2
+if dataset == 1:
+    w, t, y = load_lalonde()
+    dist = distributions.MixedDistribution([0.0], distributions.LogLogistic())
+    training_params = TrainingParams(lr=0.0005, batch_size=128, num_epochs=100, verbose=False)
+    mlp_params_y_tw = MLPParams(n_hidden_layers=2, dim_h=256)
+elif dataset == 2:
+    w, t, y = load_lalonde(rct=True)
+    dist = distributions.MixedDistribution([0.0], distributions.LogLogistic())
+    training_params = TrainingParams(lr=0.0005, batch_size=128, num_epochs=500)
+    mlp_params_y_tw = MLPParams(n_hidden_layers=2, dim_h=256)
+elif dataset == 3:
+    w, t, y = load_lalonde(obs_version='cps1')
+    dist = distributions.MixedDistribution([0.0, 25564.669921875/y.max()], distributions.LogNormal())
+    training_params = TrainingParams(lr=0.0005, batch_size=128, num_epochs=1000)
+    mlp_params_y_tw = MLPParams(n_hidden_layers=3, dim_h=512, activation=torch.nn.LeakyReLU())
+else:
+    raise(Exception('dataset {} not implemented'.format(dataset)))
+
+mlp = MLP(w, t, y,
+          training_params=training_params,
+          mlp_params_y_tw=mlp_params_y_tw,
+          binary_treatment=True, outcome_distribution=dist,
+          outcome_min=0.0, outcome_max=1.0,
+          train_prop=0.5, val_prop=0.1, test_prop=0.4,
+          seed=1, early_stop=False,
+          w_transform=preprocess.Standardize, y_transform=preprocess.Normalize)
+mlp._train()
+
+naive = y[t == 1].mean() - y[t == 0].mean()
+print('Naive ATE:', naive)
+print('Noisy ATE est (using sample_y):', mlp.noisy_ate())
+print('ATE est (using mean_y):', mlp.ate())
