5c4521853d58d09bec4f319dd0772f1ad800ae07
raghupas <raghupas@rtx1.server.mila.quebec>
2020-09-01 13:56:09 -0400

    Script to compile experiment results into csv

---
+++ compile_results.py
@@ -0,0 +1,48 @@
+# import csv
+from pathlib import Path
+import os
+import ast
+import pandas as pd
+from tqdm import tqdm
+import argparse
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--filters", nargs="+", help="filter experiments (e.g. `linear dim_h128`)"
+    )
+    parser.add_argument(
+        "--exp_dir", type=str, help="Path to experiment folder", required=True
+    )
+    parser.add_argument(
+        "--verbose", action="store_true",
+    )
+
+    arguments = parser.parse_args()
+
+    filter_criterion = arguments.filters
+
+    experiment_dir = Path(arguments.exp_dir)
+    experiments = os.listdir(experiment_dir)
+
+    results = {}
+    for exp in tqdm(experiments):
+        for word in filter_criterion:
+            if word not in exp:
+                if arguments.verbose:
+                    print(f"Omitting exp {exp}")
+                continue
+
+        exp = Path(exp)
+
+        if (experiment_dir / exp / "summary.txt").is_file():
+            with open(experiment_dir / exp / "summary.txt") as f:
+                exp_contents = f.read()
+                exp_dict = ast.literal_eval(exp_contents)
+                results[exp] = exp_dict
+
+    df = pd.DataFrame.from_dict(results).transpose()
+    df.to_csv(experiment_dir.stem + ".csv")
+
